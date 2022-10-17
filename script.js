99e92bc34e9d4956a2a7d44e60115aa3482a40a2
Marlon Couto <marloncouto199@gmail.com>
2022-10-17 19:26:03 -0300

    Conclui requisito 21
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const buttonSubmit = document.querySelector('#button-submit');
-const sendButton = document.querySelector('#submit-btn');
-const agreementButton = document.querySelector('#agreement');
-const textarea = document.querySelector('textarea');
+const buttonSubmit = document.querySelector("#button-submit");
+const sendButton = document.querySelector("#submit-btn");
+const agreementButton = document.querySelector("#agreement");
+const textarea = document.querySelector("textarea");
+const main = document.querySelector("main");
 
-buttonSubmit.addEventListener('click', () => {
-  const formName = document.querySelector('#email').value;
-  const formEmail = document.querySelector('#password').value;
-  if (formName === 'tryber@teste.com' && formEmail === '123456') {
-    alert('Olá, Tryber!');
+buttonSubmit.addEventListener("click", () => {
+  const formName = document.querySelector("#email").value;
+  const formEmail = document.querySelector("#password").value;
+  if (formName === "tryber@teste.com" && formEmail === "123456") {
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
 });
 
@@ -19,7 +20,7 @@ const disableButton = () => {
   }
 };
 
-agreementButton.addEventListener('change', () => {
+agreementButton.addEventListener("change", () => {
   if (agreementButton.checked === false) {
     sendButton.disabled = true;
   } else {
@@ -28,11 +29,47 @@ agreementButton.addEventListener('change', () => {
 });
 
 const characterCount = () => {
-  const current = document.querySelector('#current');
+  const current = document.querySelector("#current");
   current.innerText = 500 - textarea.value.length;
 };
 
-textarea.addEventListener('input', characterCount);
+textarea.addEventListener("input", characterCount);
+
+const createFormData = (data) => {
+  main.appendChild(document.createElement("div"));
+  document.querySelector("main div").id = "form-data";
+  const formData = document.querySelector("#form-data");
+  formData.style.width;
+  for (let index = 0; index < data.length; index += 1) {
+    formData.appendChild(document.createElement("p"));
+    formData.lastChild.innerText = data[index];
+  }
+};
+
+sendButton.addEventListener("click", () => {
+  const checkedSubjects = document.querySelectorAll(".subject:checked");
+  let subjects = "";
+  for (let index = 0; index < checkedSubjects.length; index += 1) {
+    if (index === checkedSubjects.length - 1) {
+      subjects += `${checkedSubjects[index].value}.`;
+    } else {
+      subjects += `${checkedSubjects[index].value}, `;
+    }
+  }
+  const data = [
+    `Nome: ${document.querySelector("#input-name").value} ${
+      document.querySelector("#input-lastname").value
+    }`,
+    `Email: ${document.querySelector("#input-email").value}`,
+    `Casa: ${document.querySelector("#house").value}`,
+    `Família: ${document.querySelector('input[name="family"]:checked').value}`,
+    `Matérias: ${subjects}`,
+    `Avaliação: ${document.querySelector('input[name="rate"]:checked').value}`,
+    `Observações: ${document.querySelector("textarea").value}`,
+  ];
+  main.firstElementChild.style.display = 'none';
+  createFormData(data);
+});
 
 window.onload = () => {
   disableButton();
